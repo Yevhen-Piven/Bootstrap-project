0a0e1ac23a3efb1671b98776c05119790de48b0d
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-06-30 17:19:03 +0200

    commit

--- Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/SecurityConfig.java
+++ Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,30 +16,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorizeRequests ->
-                authorizeRequests
-                    .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()  
-                    .anyRequest().authenticated()  
-            )
-            .formLogin(formLogin ->
-                formLogin
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/home", true)
-                    .permitAll()
-            )
-            .logout(logout ->
-                logout
-                    .permitAll()
-            );
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/", "/home", "/login").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll());
+
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder().username("user").password(passwordEncoder().encode("password")).roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = org.springframework.security.core.userdetails.User.withUsername("user")
+                .password(passwordEncoder().encode("password")).roles("USER").build();
+
+        var admin = org.springframework.security.core.userdetails.User.withUsername("admin")
+                .password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+
+        userDetailsService.createUser(user);
+        userDetailsService.createUser(admin);
+
+        return userDetailsService;
     }
 
     @Bean

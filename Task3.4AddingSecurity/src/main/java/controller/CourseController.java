9f32f1165569cdfb4879833070e9c0aad61287bc
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-06-30 21:35:08 +0200

    с

--- Task3.4AddingSecurity/src/main/java/controller/CourseController.java
+++ Task3.4AddingSecurity/src/main/java/controller/CourseController.java
@@ -3,14 +3,16 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import service.CourseService;
 
 @Controller
+@RequestMapping("/courses")
 public class CourseController {
     private CourseService courseService;
 
-    @GetMapping("/api/courses")
+    @GetMapping
     public String listCourses(Model model) {
         model.addAttribute("courses", courseService.findAll());
         return "courses";

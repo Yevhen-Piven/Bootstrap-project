5fc55ed3a52bb9c98c35fd964b783bf2d30ecad0
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-06-30 17:33:09 +0200

    commit

--- Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/SecurityConfig.java
+++ Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/SecurityConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,26 +18,5 @@ public class SecurityConfig {
                 .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll());
 
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = org.springframework.security.core.userdetails.User.withUsername("user")
-                .password(passwordEncoder().encode("password")).roles("USER").build();
-
-        var admin = org.springframework.security.core.userdetails.User.withUsername("admin")
-                .password(passwordEncoder().encode("admin")).roles("ADMIN").build();
-
-        userDetailsService.createUser(user);
-        userDetailsService.createUser(admin);
-
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }

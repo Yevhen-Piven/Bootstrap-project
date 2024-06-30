c33504b1dab44d97e6ea116df7b1420399b8151f
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-06-30 19:49:27 +0200

    c

--- Task3.4AddingSecurity/src/main/java/controller/ClassroomController.java
+++ Task3.4AddingSecurity/src/main/java/controller/ClassroomController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import entity.User;
 import service.ClassroomService;
 
 @Controller
@@ -12,6 +13,7 @@ public class ClassroomController {
 
     @GetMapping("/classrooms")
     public String listClassrooms(Model model) {
+        model.addAttribute("user", new User());
         model.addAttribute("classrooms", classroomService.findAll());
         return "classrooms";
     }

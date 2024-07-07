957a1364040ec5a613dc0cd45e61b5a534ea6a12
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-07-07 22:50:19 +0200

    committ

--- Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/BootstrapprojectApplication.java
+++ Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/BootstrapprojectApplication.java
@@ -2,9 +2,10 @@ package com.yevhenpiven.bootstrapproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
 @ComponentScan
 public class BootstrapprojectApplication {
 

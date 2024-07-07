9308035265969936800bd58e4ff4e10fd28c33bc
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-07-07 22:42:53 +0200

    controller

--- Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/BootstrapprojectApplication.java
+++ Task3.4AddingSecurity/src/main/java/com/yevhenpiven/bootstrapproject/BootstrapprojectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("controller")
+@ComponentScan
 public class BootstrapprojectApplication {
 
     public static void main(String[] args) {

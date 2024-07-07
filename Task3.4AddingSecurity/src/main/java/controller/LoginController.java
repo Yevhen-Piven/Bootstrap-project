4d5027156edd8b227f2c1c7904d76758d3e1f290
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-07-07 19:30:30 +0200

    с

--- Task3.4AddingSecurity/src/main/java/controller/LoginController.java
+++ Task3.4AddingSecurity/src/main/java/controller/LoginController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/Task3.4AddingSecurity/login")
 public class LoginController {
 
     public String login() {
